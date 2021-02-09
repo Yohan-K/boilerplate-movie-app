8bf7b75909fe09e28f0758e0165b19c0d426d8ef
김정현 <rlawjdgus843@gmail.com>
2021-02-09 20:11:11 +0900

    init

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const API_KEY = '21cbdff0ada2868f28431ee0047dd4f7';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
